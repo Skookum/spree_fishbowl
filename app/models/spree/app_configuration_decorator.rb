9d525134cb5d2d462f5f668b83f00ce02d62d2c2
Michael Porter <michael@skookum.com>
2013-05-30 20:20:42 -0400

    Add ability to enable / disable Fishbowl integration

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,5 +1,6 @@
 Spree::AppConfiguration.class_eval do
 
+  preference :enable_fishbowl, :boolean, :default => false
   preference :fishbowl_host, :string, :default => 'localhost'
   preference :fishbowl_user, :string, :default => 'admin'
   preference :fishbowl_password, :password, :default => nil
