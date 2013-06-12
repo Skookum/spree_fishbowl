47453ececcc3e71acabccad40ce378532d895330
Michael Porter <michael@skookum.com>
2013-06-12 18:06:14 -0400

    Add middleware to ensure the Fishbowl socket connection is closed at the end of the request

---
+++ lib/spree_fishbowl/middleware/close_fishbowl_connection.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+module SpreeFishbowl
+  module Middleware
+    class CloseFishbowlConnection
+
+      def initialize(app)
+        @app = app
+      end
+
+      def call(env)
+        status, headers, body = @app.call(env)
+
+        connection = SpreeFishbowl.connection
+        connection.disconnect unless connection.nil?
+
+        [status, headers, body]
+      end
+
+    end
+  end
+end
