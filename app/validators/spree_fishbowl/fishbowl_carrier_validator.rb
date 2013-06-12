4ae0da13c1abf66e59e5e9385fb5f3340ae6cf77
Michael Porter <michael@skookum.com>
2013-06-12 18:11:50 -0400

    Invert bad test on carrier match

--- app/validators/spree_fishbowl/fishbowl_carrier_validator.rb
+++ app/validators/spree_fishbowl/fishbowl_carrier_validator.rb
@@ -4,7 +4,7 @@ module SpreeFishbowl
     def validate_each(record, attribute, value)
       record.errors[attribute] << 'is not a valid carrier in Fishbowl' if (
         SpreeFishbowl.enabled? &&
-        SpreeFishbowl.client_from_config.carriers.include?(params[:name])
+        !SpreeFishbowl.client_from_config.carriers.include?(value)
       )
     end
 
