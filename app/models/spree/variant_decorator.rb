b7aff058b5eee7c7ed477d5e2b743b20da615674
Michael Porter <michael@skookum.com>
2013-06-25 22:21:13 -0400

    Allow setting inventory when Fishbowl integration is disabled

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -5,6 +5,7 @@ module Spree
     validates :sku, 'spree_fishbowl/sku_in_fishbowl' => true, :if => "sku.present?"
 
     alias_method :orig_on_hand, :on_hand
+    alias_method :orig_on_hand=, :on_hand=
 
     def on_hand
       return orig_on_hand if (!SpreeFishbowl.enabled? || !sku)
@@ -18,7 +19,9 @@ module Spree
     end
 
     def on_hand=(new_level)
-      # no-op
+      return orig_on_hand=(new_level) if !SpreeFishbowl.enabled?
+
+      # no-op otherwise
     end
 
   end
